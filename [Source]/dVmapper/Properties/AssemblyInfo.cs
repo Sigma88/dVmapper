3a92e97aeaaf3c7c0505f66355c817de43d8fbac
Sigma88 <sigmamodexpansions@outlook.com>
2016-12-17 17:23:58 +0100

    let's start

---
+++ [Source]/dVmapper/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("ExportTexturesByName")]
+[assembly: AssemblyDescription("Exports Textures By Name")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: KSPAssembly("ExportTexturesByName", 1, 0)]
+[assembly: KSPAssemblyDependency("Kopernicus", 1, 0)]
+[assembly: AssemblyVersion("1.0.0")]
+
---
+++ [Source]/dVmapper/dVmapper.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Kopernicus;
+using Kopernicus.Components;
+using Kopernicus.Configuration;
+using Kopernicus.OnDemand;
+using System;
+using System.Reflection;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+
+
+namespace SigmadVmapperPlugin
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    public class dVmapper : MonoBehaviour
+    {
+        void Start()
+        {
+            Assets.Load();
+        }
+    }
+}
