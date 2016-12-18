181b6374b607630e984d21b61eb1c434d1046c23
Sigma88 <sigmamodexpansions@outlook.com>
2016-12-18 20:13:32 +0100

    fix planet thumbnails

--- [Source]/dVmapper/Assets.cs
+++ [Source]/dVmapper/Assets.cs
@@ -16,155 +16,25 @@ namespace SigmadVmapperPlugin
     {
         public static Dictionary<string, Texture2D> planets = new Dictionary<string, Texture2D>();
         public static Dictionary<string, Texture2D> orbits = new Dictionary<string, Texture2D>();
+        public static Color background = new Color(1, 1, 1, 1);
 
         public static void Load()
         {
             // Load orbits sprites
             string[] names = new string[] { "circle_top", "circle_middle", "circle_bottom", "orbit_low", "orbit_synch", "orbit_elliptic", "orbit_flyby" };
-            foreach (string name in names)
+
+            foreach(Texture texture in Resources.FindObjectsOfTypeAll<Texture>())
             {
-                Texture2D sprite = Utility.CreateReadable(Resources.FindObjectsOfTypeAll<Texture>().First(t => t.name == ("Sigma/dVmapper/" + name)) as Texture2D);
-                if (sprite != null)
+                string name = texture.name.Replace("Sigma/dVmapper/Assets/", "");
+                if (names.Contains(name))
                 {
+                    Texture2D sprite = Utility.CreateReadable(texture as Texture2D);
                     orbits.Add(name, sprite);
                 }
             }
-
-            // Load planets sprites
-            Thumbnail();
-        }
-
-
-        static CelestialBody body = null;
-        static int size = 60;
-
-        static void Thumbnail()
-        {
-            Texture2D MainTex = Utility.CreateReadable(body.scaledBody.GetComponent<Renderer>().material.GetTexture("_MainTex") as Texture2D);
-            if (MainTex != null)
-            {
-                Texture2D thumb = new Texture2D(size, size);
-                for (int x = 0; x < size; x++)
-                {
-                    for (int y = 0; y < size; y++)
-                    {
-                        Color[] colors = MainTex.GetPixels((x * MainTex.width / (size * 2)) + (MainTex.width / 4), y * MainTex.height / (size), MainTex.width / (size * 2), MainTex.height / (size));
-                        float[] AVGcolor = { 0, 0, 0 };
-                        foreach (Color color in colors)
-                        {
-                            AVGcolor[0] += color.r;
-                            AVGcolor[1] += color.g;
-                            AVGcolor[2] += color.b;
-                        }
-                        AVGcolor[0] = AVGcolor[0] / colors.Length;
-                        AVGcolor[1] = AVGcolor[1] / colors.Length;
-                        AVGcolor[2] = AVGcolor[2] / colors.Length;
-                        Color pixel = new Color(AVGcolor[0], AVGcolor[1], AVGcolor[2]);
-                        thumb.SetPixel((size - 1) - x, y, pixel);
-                    }
-                }
-                Shape(thumb);
-            }
-        }
-
-        static void Shape(Texture2D thumb)
-        {
-            Texture2D newThumb = new Texture2D(size + 5, size + 5);
-            Clear(newThumb);
-
-            for (int x = 0; x < (size * 2); x++)
-            {
-                for (int y = 0; y < (size * 2); y++)
-                {
-                    double lat = 90 - (y * 90d / size);
-                    double lon = (x * 90d / size) - 90;
-                    Vector3 position = Utility.LLAtoECEF(lat, lon, 0.5, 0.5);
-                    double altitude = 1;
-                    if (body.pqsController != null)
-                    {
-                        altitude = body.pqsController.GetSurfaceHeight(position);
-                        if (altitude < body.Radius)
-                            altitude = body.Radius;
-                        altitude /= body.pqsController.radiusMax;
-                    }
-                    position *= (float)altitude;
-                    Vector3 plane = new Vector3(1, 0, 0);
-                    Vector3 projection = Vector3.ProjectOnPlane(position, plane);
-                    int px = (int)Math.Round(projection.z * size * 0.5);
-                    int py = (int)Math.Round(projection.y * size * 0.5);
-                    px += (size / 2);
-                    py = (size / 2) - py;
-
-                    Color color = thumb.GetPixel((int)Math.Round(x * 0.5), (int)Math.Round(y * 0.5));
-                    color.a = 1;
-                    newThumb.SetPixel(px + 2, py + 2, color);
-                }
-            }
-            Border(newThumb, 1);
-        }
-
-        static void Border(Texture2D thumb, int border)
-        {
-            Texture2D newThumb = new Texture2D(thumb.width + 4 * border, thumb.height + 4 * border);
-            Clear(newThumb);
-            newThumb.SetPixels(2 * border, 2 * border, thumb.width, thumb.height, thumb.GetPixels());
-
-            for (int x = 0; x < newThumb.width; x++)
-            {
-                for (int y = 0; y < newThumb.height; y++)
-                {
-                    float alpha = newThumb.GetPixel(x, y).a;
-                    if (alpha == 1f)
-                    {
-                        Color[] region = newThumb.GetPixels(x - border, y - border, 1 + border * 2, 1 + border * 2);
-                        for (int i = 0; i < region.Length; i++)
-                        {
-                            if (region[i].a == 0f)
-                            {
-                                Color color = newThumb.GetPixel(x, y);
-                                color.a = 0.5f;
-                                newThumb.SetPixel(x, y, color);
-                            }
-                        }
-                    }
-                }
-            }
-
-            Color[] modifications = newThumb.GetPixels(2 * border, 2 * border, thumb.width, thumb.height);
-            thumb.SetPixels(modifications);
-
-            Circle(thumb);
-        }
-        // input a 84x84 texture to Circle !!!!!!!!
-        static void Circle(Texture2D thumb)
-        {
-            Color[] top = orbits["circle_top"].GetPixels();
-            Color[] bottom =  orbits["circle_bottom"].GetPixels();
-            Color[] sprite = thumb.GetPixels();
-            for (int i = 0; i < top.Length; i++)
-            {
-                if (top[i].a > 0)
-                {
-                    sprite[i] = top[i];
-                }
-                else if (bottom[i].a > 0)
-                {
-                    sprite[i] = bottom[i];
-                }
-            }
-            thumb.SetPixels(sprite);
-            planets.Add(body.transform.name, thumb);
-        }
-
-        static void Clear(Texture2D thumb)
-        {
-            for (int x = 0; x < thumb.width; x++)
-            {
-                for (int y = 0; y < thumb.height; y++)
-                {
-                    thumb.SetPixel(x, y, new Color(0, 0, 0, 0));
-                }
-            }
+            
+            // Generate planets sprites
+            PlanetSprites.Generate();
         }
     }
 }
---
+++ [Source]/dVmapper/PlanetsSprites.cs
@@ -0,0 +1,207 @@
+﻿using UnityEngine;
+using Kopernicus;
+using Kopernicus.Components;
+using Kopernicus.Configuration;
+using Kopernicus.OnDemand;
+using System;
+using System.Reflection;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+
+namespace SigmadVmapperPlugin
+{
+    public static class PlanetSprites
+    {
+        static CelestialBody body = null;
+        static int size = 60;
+        static Texture2D MainTex = null;
+        static double longitude = 150;
+
+        public static void Generate()
+        {
+            foreach (CelestialBody cb in FlightGlobals.Bodies)
+            {
+                body = cb;
+                GetTexture();
+            }
+        }
+
+        static void GetTexture()
+        {
+            MainTex = Utility.CreateReadable(body.scaledBody.GetComponent<Renderer>().material.GetTexture("_MainTex") as Texture2D);
+            if (MainTex != null)
+            {
+                double shift = ((longitude) % 360) / 360;
+                if (shift < 0) shift += 1;
+                shift = (shift * MainTex.width);
+                Color[] block1 = MainTex.GetPixels(MainTex.width - (int)shift, 0, (int)shift, MainTex.height);
+                Color[] block2 = MainTex.GetPixels(0, 0, MainTex.width - (int)shift, MainTex.height);
+                MainTex.SetPixels(0, 0, (int)shift, MainTex.height, block1);
+                MainTex.SetPixels((int)shift, 0, MainTex.width - (int)shift, MainTex.height, block2);
+
+                Texture2D thumb = new Texture2D(size, size);
+                for (int x = 0; x < size; x++)
+                {
+                    for (int y = 0; y < size; y++)
+                    {
+                        double LAT = (y * 1d / size) * MainTex.height;
+                        double LON = (x * 1d / size) * (MainTex.width / 2d);
+                        
+                        double LATrange = MainTex.height / size;
+                        double LONrange = (MainTex.width / 2) / size;
+
+                        Color[] colors = MainTex.GetPixels((int)LON, (int)LAT, (int)LONrange, (int)LATrange);
+                        float[] AVGcolor = { 0, 0, 0 };
+                        foreach (Color color in colors)
+                        {
+                            AVGcolor[0] += color.r;
+                            AVGcolor[1] += color.g;
+                            AVGcolor[2] += color.b;
+                        }
+                        AVGcolor[0] = AVGcolor[0] / colors.Length;
+                        AVGcolor[1] = AVGcolor[1] / colors.Length;
+                        AVGcolor[2] = AVGcolor[2] / colors.Length;
+                        Color pixel = new Color(AVGcolor[0], AVGcolor[1], AVGcolor[2]);
+                        thumb.SetPixel((size - 1) - x, y, pixel);
+                    }
+                }
+                GetShape(thumb);
+            }
+        }
+
+        static void GetShape(Texture2D thumb)
+        {
+            Texture2D newThumb = new Texture2D(size + 5, size + 5);
+            Clear(newThumb);
+
+            for (int x = 0; x < (size * 2); x++)
+            {
+                for (int y = 0; y < (size * 2); y++)
+                {
+                    double lat = y * 180d / (size * 2d) - 90d;
+                    double lon = x * 180d / (size * 2d) - 90d + longitude;
+                    Vector3 position = Utility.LLAtoECEF(lat, lon, 0.5, 0.5);
+                    double altitude = 1;
+                    if (body.pqsController != null)
+                    {
+                        altitude = body.pqsController.GetSurfaceHeight(position);
+                        if (altitude < body.Radius)
+                            altitude = body.Radius;
+                        altitude /= body.pqsController.radiusMax;
+                    }
+                    position *= (float)altitude;
+
+                    Vector3 plane = Utility.LLAtoECEF(0, longitude, 0.5, 0.5);
+                    Vector3 projection = Vector3.ProjectOnPlane(position, plane);
+                    
+                    double py = projection.y;
+                    projection.y = 0;
+
+                    double px = projection.magnitude;
+                    if (lon < longitude)
+                        px *= -1;
+
+                    // From 0 to size
+                    px = (px + 1) / 2 * size;
+                    py = (py + 1) / 2 * size;
+
+                    Color color = thumb.GetPixel(x / 2, y / 2);
+                    color.a = 1;
+                    newThumb.SetPixel((int)px + 2, newThumb.height - (int)py - 3, color);
+                }
+            }
+            UnityEngine.Object.DestroyImmediate(thumb);
+            SetBorder(newThumb, 1);
+        }
+
+        static void SetBorder(Texture2D thumb, int border)
+        {
+            Color[] colors = thumb.GetPixels();
+            Texture2D newThumb = new Texture2D(84, 84);
+            Clear(newThumb);
+            newThumb.SetPixels((int)((84.5 - thumb.width) / 2), (int)((84.5 - thumb.height) / 2), thumb.width, thumb.height, colors);
+
+            for (int x = 0; x < newThumb.width; x++)
+            {
+                for (int y = 0; y < newThumb.height; y++)
+                {
+                    float alpha = newThumb.GetPixel(x, y).a;
+                    if (alpha == 1f)
+                    {
+                        Color[] region = newThumb.GetPixels(x - border, y - border, 1 + border * 2, 1 + border * 2);
+                        for (int i = 0; i < region.Length; i++)
+                        {
+                            if (region[i].a == 0f)
+                            {
+                                Color color = newThumb.GetPixel(x, y);
+                                color = Color.Lerp(Assets.background, color, 0.5f);
+                                color.a = 1;
+                                newThumb.SetPixel(x, y, color);
+                            }
+                        }
+                    }
+                }
+            }
+            UnityEngine.Object.DestroyImmediate(thumb);
+            AddCircle(newThumb);
+        }
+
+        static void AddCircle(Texture2D thumb)
+        {
+            Color[] top = Assets.orbits["circle_top"].GetPixels();
+            Color[] bottom = Assets.orbits["circle_bottom"].GetPixels();
+
+            Color[] sprite = thumb.GetPixels();
+
+            for (int i = 0; i < top.Length; i++)
+            {
+                if (top[i].a > 0)
+                {
+                    sprite[i] = Color.Lerp(Assets.background, top[i], top[i].a);
+                    sprite[i].a = 1;
+                }
+                else if (bottom[i].a > 0)
+                {
+                    sprite[i] = Color.Lerp(Assets.background, bottom[i], bottom[i].a);
+                    sprite[i].a = 1;
+                }
+            }
+            thumb.SetPixels(sprite);
+            Fill(thumb);
+            Assets.planets.Add(body.transform.name, thumb);
+        }
+
+        static void Clear(Texture2D thumb)
+        {
+            for (int x = 0; x < thumb.width; x++)
+            {
+                for (int y = 0; y < thumb.height; y++)
+                {
+                    thumb.SetPixel(x, y, new Color(0, 0, 0, 0));
+                }
+            }
+        }
+
+        static void Fill(Texture2D thumb)
+        {
+            Color[] pixels = thumb.GetPixels();
+            for (int i = 0; i < pixels.Length; i++)
+            {
+                if (pixels[i].a == 0)
+                {
+                    pixels[i] = Assets.background;
+                }
+            }
+            thumb.SetPixels(pixels);
+        }
+
+        static void Print(Texture2D thumb, string name)
+        {
+            Debug.Log("SigmaLog: Thumbnail completed");
+            byte[] png = thumb.EncodeToPNG();
+            File.WriteAllBytes(Application.dataPath + "/../GameData/Sigma/dVmapper/Assets/Sprites/" + body.transform.name + "_" + name + ".png", png);
+            Debug.Log("SigmaLog: Thumbnail saved");
+        }
+    }
+}
